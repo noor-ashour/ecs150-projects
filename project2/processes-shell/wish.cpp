a217ac44c855273177d9f9172d8dc25f5228a5ac
Noor A <55614986+noor-ashour@users.noreply.github.com>
2024-10-11 23:45:03 +0000

    Redirection

--- project2/processes-shell/wish.cpp
+++ project2/processes-shell/wish.cpp
@@ -6,17 +6,23 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <vector>
 #include <sstream>
+#include <algorithm> // for find() function
 
 using namespace std;
-// TODO work on redirection
+// TODO better way for redirection: vector<string> redirectionFile
+    // to check for multiple files or no files, use .size()
 // NEXT parallel commands
-// NEXT allow no-whitespace with &
+// NEXT allow no-whitespace with & and <
 // NEXT batch mode
 void writeError(); 
 int interactiveMode(string);
-const char* checkPath(vector<string>, vector<const char*>);
+const char* checkPath(string, vector<const char*>);
 
 vector<const char*>pathsList = {"/bin/"};
 const char* ERROR = strdup("ERROR");
@@ -41,7 +47,6 @@ int main(int argc, char *argv[]) {
         writeError();
         exit(1);
     }
-    
     exit(0);
 }
 
@@ -54,18 +59,37 @@ void writeError() {
 // Run shell and let user input commands, return 1 if any error occurs
 int interactiveMode(string str) {
     vector<string> line;
-
+    bool redirect = false;
     stringstream s(str);
     string w;
-    
+
     while (s >> w) {
-        line.push_back(w); 
+        // Check for redirection
+        if (w == ">") {
+            redirect = true;
+            line.push_back(">");
+        } else line.push_back(w);
+    }
+
+    // If redirect, set file name and remove from line
+    string redirectionFile;
+    if (redirect) {
+        redirectionFile = line.back();
+        if (redirectionFile == "" || redirectionFile == ">")
+            return 1;
+        else {
+            line.erase(line.end() - 2, line.end()); // remove file name and >
+            // Check for multiple files
+            auto it = find(line.begin(), line.end(), ">");
+            if (it != line.end())
+                return 1;
+        }
     }
 
     char* arr[line.size() + 1];
 
     for (size_t i = 0; i < line.size(); i++)
-        arr[i] = strdup(w.c_str());
+        arr[i] = strdup(line[i].c_str());
 
     // Add terminating null char
     arr[line.size()] = NULL;
@@ -90,7 +114,7 @@ int interactiveMode(string str) {
         // Check command path
         const char* path;
         if (pathsList[0] != NULL) {
-            path = checkPath(line, pathsList);
+            path = checkPath(line.front(), pathsList);
             if (path == ERROR)
                 return 1;
         } else return 1; // Will not recognize non-built-in commands if pathsList is empty
@@ -99,6 +123,16 @@ int interactiveMode(string str) {
 
         if (pid == 0) {
             // child
+
+            // Check for ">" redirection
+            if (redirect) {
+                int fd = open(redirectionFile.c_str(), O_WRONLY + O_TRUNC + O_CREAT);
+                if (fd == -1)
+                    return 1;
+                if (dup2(fd, STDOUT_FILENO) == -1)
+                    return 1;
+            }
+
             int ret_exec = execv(path, arr);
 
             if (ret_exec == -1) // execv error
@@ -115,14 +149,14 @@ int interactiveMode(string str) {
     return 0;
 }
 
-// Use access() to check if the command path is valid, else return ERROR
-const char* checkPath(vector<string> v, vector<const char*> pathsList) {
+// Use access() to check if the command path(s) is valid, else return ERROR
+const char* checkPath(string cmd, vector<const char*> pathsList) {
     const char* path = ERROR;
     const char* tempPath;
     string pathStr;
 
     for (const char* p : pathsList) {
-        pathStr = p + v.front();
+        pathStr = p + cmd;
         tempPath = strdup(pathStr.c_str());
         
         if (access(tempPath, X_OK) == 0) {
