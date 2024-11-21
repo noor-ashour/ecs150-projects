08ddf1798827ba13cff5618ca7a87f2c71a4ea2d
Noor A <55614986+noor-ashour@users.noreply.github.com>
2024-11-21 07:05:36 +0000

    Working on stat and lookup and helper functions. I don't know what's going on

--- project4/gunrock_web/LocalFileSystem.cpp
+++ project4/gunrock_web/LocalFileSystem.cpp
@@ -5,20 +5,22 @@
 
 #include "LocalFileSystem.h"
 #include "ufs.h"
+#include <string.h>
 
 using namespace std;
-
 
 LocalFileSystem::LocalFileSystem(Disk *disk) {
   this->disk = disk;
 }
 
 void LocalFileSystem::readSuperBlock(super_t *super) {
-
+  //Disk *disk = this->disk;
+  disk->readBlock(0, super);
 }
 
 void LocalFileSystem::readInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
-
+  //Disk *disk = this->disk;
+  disk->readBlock(super->inode_bitmap_addr, inodeBitmap);
 }
 
 void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitmap) {
@@ -26,7 +28,8 @@ void LocalFileSystem::writeInodeBitmap(super_t *super, unsigned char *inodeBitma
 }
 
 void LocalFileSystem::readDataBitmap(super_t *super, unsigned char *dataBitmap) {
-
+  //Disk *disk = this->disk;
+  disk->readBlock(super->data_bitmap_addr, dataBitmap);
 }
 
 void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap) {
@@ -34,7 +37,8 @@ void LocalFileSystem::writeDataBitmap(super_t *super, unsigned char *dataBitmap)
 }
 
 void LocalFileSystem::readInodeRegion(super_t *super, inode_t *inodes) {
-
+  //Disk *disk = this->disk;
+  disk->readBlock(super->inode_region_addr, inodes);
 }
 
 void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes) {
@@ -42,14 +46,59 @@ void LocalFileSystem::writeInodeRegion(super_t *super, inode_t *inodes) {
 }
 
 int LocalFileSystem::lookup(int parentInodeNumber, string name) {
+  inode_t parent_inode;
+
+  // Read the parent inode given its number
+  if (stat(parentInodeNumber, &parent_inode) != 0) {
+    return -EINVALIDINODE;
+  }
+
+  //for (int i = 0; i < pare)
+  
   return 0;
 }
 
 int LocalFileSystem::stat(int inodeNumber, inode_t *inode) {
+  super_t superBlock;
+  readSuperBlock(&superBlock);
+
+  int inodes_per_block = UFS_BLOCK_SIZE / sizeof(inode_t);
+
+  inode_t *inodes = new inode_t[inodes_per_block];
+  readInodeRegion(&superBlock, inodes);
+  
+  int offset = inodeNumber % inodes_per_block;
+
+  inode_t *target_inode = &inodes[offset];
+
+  inode->type = target_inode->type;
+  inode->size = target_inode->size;
+
+  for (int i = 0; i < sizeof(target_inode->direct); i++) {
+    inode->direct[i] = target_inode->direct[i];
+  }
+
+  delete[] inodes;
   return 0;
 }
 
 int LocalFileSystem::read(int inodeNumber, void *buffer, int size) {
+  super_t superBlock;
+  readSuperBlock(&superBlock);
+
+  //inode_t inodeTable[num_inodes];
+  //inode_t inode = inodeTable[inodeNumber];
+  
+  int pos = 0;
+  //int index = pos / UFS_BLOCK_SIZE;
+  int offset = pos % UFS_BLOCK_SIZE;
+  //int blockNum = inode.direct[index];
+  
+  char block[UFS_BLOCK_SIZE];
+	// TODO readDataBitmap(blockNum, block);
+	memcpy(buffer, block + offset, size);
+
+  // TODO on success, return number of bytes read
   return 0;
 }
 
